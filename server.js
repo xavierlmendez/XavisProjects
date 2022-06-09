2ab44fba2f80bad7aa0fd11370a3b12995f83dd1
Xavier Mendez <xluis10.2@gmail.com>
2022-06-09 18:23:20 -0500

    pull/push01

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use('/', function (req, res) {
     //__dirname : It will resolve to your project folder.
 });
 const server = http.createServer(app);
-const port = 3001;
+const port = 80;
 server.listen(port);
 console.debug('Server listening on port ' + port);
