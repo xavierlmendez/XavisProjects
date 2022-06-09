9302916a7f48f1eaeeebbfbb812495e4254c5d42
Xavier Mendez <44298432+xavierlmendez@users.noreply.github.com>
2022-06-09 17:09:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // JavaScript source code
 const http = require('http');
-const express = require('XavisProjects/express');
+const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.json());
-app.use(express.static("XavisProjects/express"));
+app.use(express.static("express"));
 // default URL for website
 app.use('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/XavisProjects/express/index.html'));
+    res.sendFile(path.join(__dirname + '/express/index.html'));
     //__dirname : It will resolve to your project folder.
 });
 const server = http.createServer(app);
